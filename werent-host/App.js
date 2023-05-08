a4255b1162a6874fd2d9259165bd7e5268ac7a6b
Emre Karatas <85746327+emre-karatas@users.noreply.github.com>
2023-05-08 12:13:10 +0300

    Add files via upload

---
+++ werent-host/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HostRentingProperty from './HostRentingProperty';
+
+function App() {
+  return (
+    <div>
+      <HostRentingProperty />
+    </div>
+  );
+}
+
+export default App;
