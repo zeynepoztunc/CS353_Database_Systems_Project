a0277856901c3459ca0d7c8a584f54bde73e0ce0
Emre Karatas <85746327+emre-karatas@users.noreply.github.com>
2023-05-08 23:59:54 +0300

    Add files via upload

--- werent-host/src/App.js
+++ werent-host/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HostRentingProperty from './HostRentingProperty';
 import HostRentingRoomDetails from './HostRentingRoomDetails';
+import HostRentingRoomLocation from './HostRentingRoomLocation';
+import HostRentingRoomPricing from './HostRentingRoomPricing';
 
 function App() {
   return (
     <Routes>
       <Route exact path='/' element ={<HostRentingProperty/>} />
       <Route exact path='/HostRentingRoomDetails' element={<HostRentingRoomDetails/>} />
+      <Route exact path='/HostRentingRoomLocation' element={<HostRentingRoomLocation/>} />
+      <Route exact path='/HostRentingRoomPricing' element={<HostRentingRoomPricing/>} />
     </Routes>
   );
 }
