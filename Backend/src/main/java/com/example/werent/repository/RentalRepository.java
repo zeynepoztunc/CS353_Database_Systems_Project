855b0c5e155c5c774a73b2874f90ab5caf0e5713
Emre Karatas <85746327+emre-karatas@users.noreply.github.com>
2023-06-03 00:57:26 +0300

    changes in customer side + host side

--- Backend/src/main/java/com/example/werent/repository/RentalRepository.java
+++ Backend/src/main/java/com/example/werent/repository/RentalRepository.java
@@ -1,6 +1,7 @@
 package com.example.werent.repository;
 
 import com.example.werent.entity.RentalDTO;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -10,6 +11,8 @@ import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class RentalRepository {
@@ -58,6 +61,11 @@ public class RentalRepository {
 
         String sql = "UPDATE \"Rental\" SET  \"host-selected-rental-start-date\" = ?, \"host-selected-rental-end-date\" = ? WHERE \"rental-id\" = ?";
         jdbcTemplate.update(sql, startDateTimestamp, endDateTimestamp, id);
+    }
+
+    public List<Map<String, Object>> getAllRentals(Integer id) {
+        String sql = "SELECT * FROM \"Rental\" WHERE \"host-id\" = ?";
+        return jdbcTemplate.queryForList(sql, id);
     }
 
 
