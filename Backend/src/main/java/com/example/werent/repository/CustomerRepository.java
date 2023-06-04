f334ddb4c493e760b6d5a926a0b377665e0df1fa
Zeynep Selcen Öztunç <85746781+zeynepoztunc@users.noreply.github.com>
2023-06-04 18:19:39 +0300

    images added

--- Backend/src/main/java/com/example/werent/repository/CustomerRepository.java
+++ Backend/src/main/java/com/example/werent/repository/CustomerRepository.java
@@ -26,7 +26,7 @@ public class CustomerRepository {
     public List<Map<String, Object>> listRentalsForCustomer(int userId){
         //int customerId = searcher.getUserId();
         System.out.println("Listing all rentals for the customer (with id = " + userId + ")...");
-        String sqlListRentalsForCustomer = "SELECT rental.\"rental-id\", rental.\"rental-name\", rental.\"daily-price\", rental.rating, rental.city, rental.\"earthquake-support\", rental.couchsurfing, CASE WHEN  wishlists.\"rental-id\" IS NULL THEN 0 ELSE 1 END AS \"is-favorited\" FROM \"Rental\" rental LEFT JOIN ( SELECT \"rental-id\" FROM \"Wishlists\" wishlists WHERE \"user-id\" = ? ) AS wishlists ON rental.\"rental-id\" = wishlists.\"rental-id\"";
+        String sqlListRentalsForCustomer = "SELECT rental.\"rental-id\", rental.\"image-path\",rental.\"rental-name\", rental.\"daily-price\", rental.rating, rental.city, rental.\"earthquake-support\", rental.couchsurfing, CASE WHEN  wishlists.\"rental-id\" IS NULL THEN 0 ELSE 1 END AS \"is-favorited\" FROM \"Rental\" rental LEFT JOIN ( SELECT \"rental-id\" FROM \"Wishlists\" wishlists WHERE \"user-id\" = ? ) AS wishlists ON rental.\"rental-id\" = wishlists.\"rental-id\"";
 
         List<Map<String, Object>> rows = jdbcTemplate.queryForList(sqlListRentalsForCustomer, userId);
         if (rows.isEmpty()){
